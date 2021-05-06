1978c03a6c5379c79a6394135d85812e016bc529
leyangjie <leyangjie@fenxianglife.com>
2021-05-06 16:24:11 +0800

    添加数据库配置

---
+++ sql/ddl.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE `gulimall_oms` CHARACTER SET 'utf8mb4';
+CREATE DATABASE `gulimall_pms` CHARACTER SET 'utf8mb4';
+CREATE DATABASE `gulimall_sms` CHARACTER SET 'utf8mb4';
+CREATE DATABASE `gulimall_ums` CHARACTER SET 'utf8mb4';
+CREATE DATABASE `gulimall_wms` CHARACTER SET 'utf8mb4';
